5581e70e28d333ac91362393f21dc95c8da8e26a
Ash Ryan <ash@ashryanbeats.com>
2015-07-10 23:25:02 -0400

    Story activity layout and loadPage method.

--- app/src/main/java/ashryanbeats/com/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/ashryanbeats/com/interactivestory/ui/StoryActivity.java
@@ -2,17 +2,28 @@ package ashryanbeats.com.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ashryanbeats.com.interactivestory.R;
 import ashryanbeats.com.interactivestory.model.Page;
+import ashryanbeats.com.interactivestory.model.Story;
 
 
 public class StoryActivity extends Activity {
 
     private Page[] mPages;
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,22 @@ public class StoryActivity extends Activity {
             name = "friend";
         }
         Log.d(TAG, name);
+
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
     }
 
+    private void loadPage(){
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        mTextView.setText(page.getText());
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
+    }
 }
